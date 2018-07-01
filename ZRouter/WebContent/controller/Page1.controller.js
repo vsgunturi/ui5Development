dc59de0afe3f9b75879e367decc67dc42c57578b
100055259 <100055259@USSTP00022077.corp.JABIL.ORG>
2018-07-01 17:33:54 -0400

    Second commit......

--- ZRouter/WebContent/controller/Page1.controller.js
+++ ZRouter/WebContent/controller/Page1.controller.js
@@ -5,9 +5,9 @@ sap.ui.controller("gvsRouter.controller.Page1", {
 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
 * @memberOf view.Page1
 */
-//	onInit: function() {
-//
-//	},
+	onInit: function() {
+
+	},
 
 /**
 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
